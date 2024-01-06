eeb593eb88dce24c8ef3ea20a58d6e2ed83b8898
wahiba <wahiba123@gmail.com>
2024-01-06 20:15:05 +0500

    first commit

---
+++ Connection.php
@@ -0,0 +1,20 @@
+<?php
+
+define('id','localhost');
+define('First name','root' );
+define('Email','My_admin');
+define('Password','');
+
+
+$con=mysqli_connect(id,First name,Email,Password); 
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ stringfunction/ArraySort.php
@@ -0,0 +1,18 @@
+<?php
+
+//Array Sort Function In Php
+
+$Myarray=array("Ayesha","Umaima","Sana","Hafsa","Shiza");
+//$Myarray=array(45,98,76,54,39);
+//echo $Myarray;
+//rsort($Myarray);
+foreach($Myarray as $names)
+{
+    echo $names . "<br>";
+}
+
+
+
+
+
+?>
